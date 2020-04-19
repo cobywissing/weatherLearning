d722fc0f442749c01efe10bde3b9d55a2870ec43
Coby Wissing <60360589+cobywissing@users.noreply.github.com>
2020-04-19 19:36:53 -0400

    Add files via upload

--- testcsv.py
+++ testcsv.py
@@ -6,36 +6,50 @@ import pandas as pd
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn import tree
 from sklearn.datasets import load_iris
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 import os
 #the below line had to be added due to issues with graphviz conda files see the following link: https://datascience.stackexchange.com/questions/37428/graphviz-not-working-when-imported-inside-pydotplus-graphvizs-executables-not
 os.environ['PATH'] = os.environ['PATH']+';'+os.environ['CONDA_PREFIX']+r"\Library\bin\graphviz"
 #C:\Users\cobyw\Desktop\homework\machine learning\dataset.csv
 enc = OneHotEncoder(handle_unknown='ignore')
-with open('dataset.csv', newline='') as csvfile:
-    spamreader = c.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(' '.join(row))
-train_dataset = pd.read_csv(r"C:\Users\cobyw\Desktop\homework\machine learning\US_WeatherEvents_2016-2019Alterded.csv")
-
-print(train_dataset.shape)
+#with open('dataset.csv', newline='') as csvfile:
+#    spamreader = c.reader(csvfile, delimiter=' ', quotechar='|')
+#    for row in spamreader:
+#        print(' '.join(row))
+df = pd.read_csv(r"C:\Users\cobyw\Desktop\homework\machine learning\IsWeather.csv")
+lb = preprocessing.LabelEncoder()
+lf = preprocessing.LabelEncoder()
+#y_one_hot_encoder = LabelBinarizer()
+print(df.shape)
 #print(train_dataset.head())
 #print(test_dataset.shape)
-X = train_dataset.drop(['EventId','Type','Severity','StartTime(UTC)','EndTime(UTC)'], axis=1)
-Y = train_dataset['AirportCode']
+X_columns = ['AirportCode','month','day', 'year']
+y_column = ['weather']
 
 X_data = df[X_columns].to_numpy()
-y_data = df[y_column].to_numpy().reshape((-1, 1))
-X_data = X_one_hot_encoder.fit_transform(X_data)
-y_data = y_one_hot_encoder.fit_transform(y_data).ravel()
-X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2)
+y_data = df[y_column].to_numpy()
+X_data = X_data.tolist()
+y_data = y_data.tolist()
+print(X_data[0])
+print(y_data[0])
 
+
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2)
+#lb.fit(X_train)
+#lf.fit(y_data)
+#lb.transform(X_train)
+#lf.transform(y_data)
+
+enc.fit(X_data)
+print("transforming X's dataset on : ", enc.categories_, "\n")
+X_train = enc.transform(X_train).toarray()
+X_test = enc.transform(X_test).toarray()
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X_train,y_train)
 y_pred = clf.predict(X_test)
-print("Projected Y_test values \n", y_pred, "\n")
-print("Actual Y_test values:\n",y_test, "\n")
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 
