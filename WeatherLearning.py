acee311e3da643ebd6b1811401ac6dfefad83a93
Coby Wissing <60360589+cobywissing@users.noreply.github.com>
2020-05-01 13:36:37 -0400

    Add files via upload

--- WeatherLearning.py
+++ WeatherLearning.py
@@ -17,55 +17,56 @@ def processFlightData(data):
         ['Month', 'Day', 'Origin_Airport', 'WeatherDelay']) #
 
 
-# loading the smaller 2017 flight data
-flight2017 = load_data('flight-delays/fl_samp.csv')
-flight2017Processed = processFlightData(flight2017)
+def main():
+        # loading the smaller 2017 flight data
+    flight2017 = load_data('flight-delays/fl_samp.csv')
+    flight2017Processed = processFlightData(flight2017)
 
-# loading the other flight data
-flightData = load_data('flight-delays/flight.csv')
-flightDateProcessed = processFlightData(flightData)
+    # loading the other flight data
+    flightData = load_data('flight-delays/flight.csv')
+    flightDateProcessed = processFlightData(flightData)
 
-# Combine the two datasets
-frames = [flight2017Processed, flightDateProcessed]
-combinedFlightData = pd.concat(frames)
+    # Combine the two datasets
+    frames = [flight2017Processed, flightDateProcessed]
+    combinedFlightData = pd.concat(frames)
 
-print(combinedFlightData.loc[:, "Origin_Airport"].value_counts())
+    print(combinedFlightData.loc[:, "Origin_Airport"].value_counts())
 
-# preprossess data
-# Establish variables
-scalar = StandardScaler()
-ct = ColumnTransformer(
-    [("onehot", OneHotEncoder(sparse=False), ["Month", "Day", "Origin_Airport"])]
-)
+    # preprossess data
+    # Establish variables
+    scalar = StandardScaler()
+    ct = ColumnTransformer(
+        [("onehot", OneHotEncoder(sparse=False), ["Month", "Day", "Origin_Airport"])]
+    )
 
-# Set the X and Y for the datasets
-X = combinedFlightData.loc[:, "Month":"Origin_Airport"] #
-y = combinedFlightData["WeatherDelay"].values
+    # Set the X and Y for the datasets
+    X = combinedFlightData.loc[:, "Month":"Origin_Airport"] #
+    y = combinedFlightData["WeatherDelay"].values
 
-ct.fit(X)
-X_trans = ct.transform(X)
+    ct.fit(X)
+    X_trans = ct.transform(X)
 
-# Split the dataset into Test, Train, and Validate
-X_train_full, x_test, Y_train_full, y_test = train_test_split(X_trans, y, test_size=0.2)
-X_train, X_valid, Y_train, Y_valid = train_test_split(X_train_full, Y_train_full, test_size=0.2)
+    # Split the dataset into Test, Train, and Validate
+    X_train_full, x_test, Y_train_full, y_test = train_test_split(X_trans, y, test_size=0.2)
+    X_train, X_valid, Y_train, Y_valid = train_test_split(X_train_full, Y_train_full, test_size=0.2)
 
-# Set up model
-model = keras.models.Sequential([
-    keras.layers.Dense(500, activation="relu", input_shape=X_train.shape[1:]),
-    keras.layers.Dense(100, activation="relu"),
-    keras.layers.Dense(50, activation="relu"),
-    keras.layers.Dense(1)
-])
-model.compile(loss="mean_squared_error", optimizer="adam", metrics=["mean_squared_error", "accuracy"])
+    # Set up model
+    model = keras.models.Sequential([
+        keras.layers.Dense(500, activation="relu", input_shape=X_train.shape[1:]),
+        keras.layers.Dense(100, activation="relu"),
+        keras.layers.Dense(50, activation="relu"),
+        keras.layers.Dense(1)
+    ])
+    model.compile(loss="mean_squared_error", optimizer="adam", metrics=["mean_squared_error", "accuracy"])
 
-#train
-history = model.fit(X_train, Y_train, epochs=100,
-                    validation_data=(X_valid, Y_valid))
+    #train
+    history = model.fit(X_train, Y_train, epochs=100,
+                        validation_data=(X_valid, Y_valid))
 
-# evaluate
-model_test = model.evaluate(x_test, y_test)
+    # evaluate
+    model_test = model.evaluate(x_test, y_test)
 
-# Plot results
-pd.DataFrame(history.history).plot(figsize=(8, 5))
-plt.grid(True)
-plt.show()
+    # Plot results
+    pd.DataFrame(history.history).plot(figsize=(8, 5))
+    plt.grid(True)
+    plt.show()
