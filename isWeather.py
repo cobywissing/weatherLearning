5db06b4b1a5f4371a69ad2053c0762bc44423215
Coby Wissing <60360589+cobywissing@users.noreply.github.com>
2020-05-01 13:27:26 -0400

    Add files via upload

--- isWeather.py
+++ isWeather.py
@@ -9,67 +9,37 @@ from sklearn.datasets import load_iris
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
-import os
-#the below line had to be added due to issues with graphviz conda files see the following link: https://datascience.stackexchange.com/questions/37428/graphviz-not-working-when-imported-inside-pydotplus-graphvizs-executables-not
-os.environ['PATH'] = os.environ['PATH']+';'+os.environ['CONDA_PREFIX']+r"\Library\bin\graphviz"
-#C:\Users\cobyw\Desktop\homework\machine learning\dataset.csv
-enc = OneHotEncoder(handle_unknown='ignore')
-#with open('dataset.csv', newline='') as csvfile:
-#    spamreader = c.reader(csvfile, delimiter=' ', quotechar='|')
-#    for row in spamreader:
-#        print(' '.join(row))
-df = pd.read_csv(r"C:\Users\cobyw\Desktop\homework\machine learning\IsWeather.csv")
-lb = preprocessing.LabelEncoder()
-lf = preprocessing.LabelEncoder()
-#y_one_hot_encoder = LabelBinarizer()
-print(df.shape)
-print(df.head())
-#print(test_dataset.shape)
-X_columns = ['month','day', 'year']#'AirportCode'
-y_column = ['weather']
+def main():
+    enc = OneHotEncoder(handle_unknown='ignore')
+    df = pd.read_csv(r"IsWeather.csv")
+    lb = preprocessing.LabelEncoder()
+    lf = preprocessing.LabelEncoder()
+    #y_one_hot_encoder = LabelBinarizer()
+    print(df.shape)
+    print(df.head())
+    #print(test_dataset.shape)
+    X_columns = ['month','day', 'year']#'AirportCode'
+    y_column = ['weather']
 
-X_data = df[X_columns].to_numpy()
-y_data = df[y_column].to_numpy()
-X_data = X_data.tolist()
-y_data = y_data.tolist()
-print(X_data[0])
-print(y_data[0])
+    X_data = df[X_columns].to_numpy()
+    y_data = df[y_column].to_numpy()
+    X_data = X_data.tolist()
+    y_data = y_data.tolist()
+    print(X_data[0])
+    print(y_data[0])
 
 
-X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2)
-#lb.fit(X_train)
-#lf.fit(y_data)
-#lb.transform(X_train)
-#lf.transform(y_data)
+    X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2)
 
-enc.fit(X_data)
-print("transforming X's dataset on : ", enc.categories_, "\n")
-X_train = enc.transform(X_train).toarray()
-X_test = enc.transform(X_test).toarray()
+    enc.fit(X_data)
+    print("transforming X's dataset on : ", enc.categories_, "\n")
+    X_train = enc.transform(X_train).toarray()
+    X_test = enc.transform(X_test).toarray()
 
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X_train,y_train)
-y_pred = clf.predict(X_test)
-print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(X_train,y_train)
+    y_pred = clf.predict(X_test)
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
 
-#dot_data = tree.export_graphviz(clf,
-#                                feature_names=data_feature_names,
-#                                out_file=None,
-#                                filled=True,
-#                                rounded=True)
-#graph = pydotplus.graph_from_dot_data(dot_data)
-#colors = ('turquoise', 'orange')
-#edges = collections.defaultdict(list)
-
-#for edge in graph.get_edge_list():
-#    edges[edge.get_source()].append(int(edge.get_destination()))
-
-#for edge in edges:
-#    edges[edge].sort()    
-#    for i in range(2):
-#        dest = graph.get_node(str(edges[edge][i]))[0]
-#        dest.set_fillcolor(colors[i])
-
-
-#graph.write_pdf(r'C:\Users\cobyw\Desktop\homework\machine learning\tree.pdf')
+main()
