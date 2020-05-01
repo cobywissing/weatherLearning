1cc99d67a0253660ad67130650b0cc843970c1b1
blairaweaver <blairaweaver@yahoo.com>
2020-05-01 13:13:38 -0400

    Cleaned up the WeatherLearning.py file

--- WeatherLearning.py
+++ WeatherLearning.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -19,47 +17,30 @@ def processFlightData(data):
         ['Month', 'Day', 'Origin_Airport', 'WeatherDelay']) #
 
 
-
-
-# loading the 2015 flight data
-# flight2015 = load_data('2015-flight-delays/flights.csv')
-
 # loading the smaller 2017 flight data
 flight2017 = load_data('flight-delays/fl_samp.csv')
 flight2017Processed = processFlightData(flight2017)
-# print(flight2017Processed.head())
-# print(flight2017Processed["WeatherDelay"].max())
 
 # loading the other flight data
 flightData = load_data('flight-delays/flight.csv')
 flightDateProcessed = processFlightData(flightData)
-# print(flightDateProcessed.head())
-
-# loading the 2015 flight data
-# flight2015 = load_data('2015-flight-delays/flights.csv')
-# flight2015Processed = processFlightData(flight2015)
-# flight2015Processed = flight2015Processed.dropna()
-# print(flight2015Processed.head())
 
 # Combine the two datasets
-frames = [flight2017Processed, flightDateProcessed] #, flight2015Processed
+frames = [flight2017Processed, flightDateProcessed]
 combinedFlightData = pd.concat(frames)
-# new_df = combinedFlightData[combinedFlightData['WeatherDelay'] > 0]
-# print(new_df.head())
-# combinedFlightData = new_df
+
+print(combinedFlightData.loc[:, "Origin_Airport"].value_counts())
 
 # preprossess data
 # Establish variables
 scalar = StandardScaler()
 ct = ColumnTransformer(
-    [("onehot", OneHotEncoder(sparse=False), ["Month", "Day", "Origin_Airport"])] #
+    [("onehot", OneHotEncoder(sparse=False), ["Month", "Day", "Origin_Airport"])]
 )
 
 # Set the X and Y for the datasets
 X = combinedFlightData.loc[:, "Month":"Origin_Airport"] #
 y = combinedFlightData["WeatherDelay"].values
-
-# print(X.head())
 
 ct.fit(X)
 X_trans = ct.transform(X)
@@ -67,7 +48,6 @@ X_trans = ct.transform(X)
 # Split the dataset into Test, Train, and Validate
 X_train_full, x_test, Y_train_full, y_test = train_test_split(X_trans, y, test_size=0.2)
 X_train, X_valid, Y_train, Y_valid = train_test_split(X_train_full, Y_train_full, test_size=0.2)
-
 
 # Set up model
 model = keras.models.Sequential([
